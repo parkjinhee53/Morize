58d6ffb41f9ec3f76a6bc2bd25962647b4dee2da
hanni66 <72500673+hanni66@users.noreply.github.com>
2021-12-28 10:55:47 +0900

    morize

---
+++ Morize/Shared/MorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorizeApp.swift
+//  Shared
+//
+//  Created by 김하은 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
