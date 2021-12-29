43c57a57e930836dc12b5c91f3c58445b6f63333
MojitoBar <wnehdtjr5@gmail.com>
2021-12-29 10:57:01 +0900

    Feat: 영단어 4*4 랜덤으로 배치

---
+++ Morize/Shared/ds/View/MiniGame.swift
@@ -0,0 +1,20 @@
+//
+//  MiniGame.swift
+//  Morize (iOS)
+//
+//  Created by judongseok on 2021/12/29.
+//
+
+import SwiftUI
+
+struct MiniGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiniGame_Previews: PreviewProvider {
+    static var previews: some View {
+        MiniGame()
+    }
+}
---
+++ Morize/Shared/ds/ViewModel/MiniGameVM.swift
@@ -0,0 +1,8 @@
+//
+//  MiniGameVM.swift
+//  Morize (iOS)
+//
+//  Created by judongseok on 2021/12/29.
+//
+
+import Foundation
