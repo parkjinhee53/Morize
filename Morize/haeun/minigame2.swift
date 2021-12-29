3273f8e5a7bba718c19cddba9c5559f10d650526
hanni66 <72500673+hanni66@users.noreply.github.com>
2021-12-29 14:45:46 +0900

    test

---
+++ Morize/haeun/minigame2.swift
@@ -0,0 +1,20 @@
+//
+//  minigame2.swift
+//  Morize (iOS)
+//
+//  Created by 김하은 on 2021/12/29.
+//
+
+import SwiftUI
+
+struct minigame2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct minigame2_Previews: PreviewProvider {
+    static var previews: some View {
+        minigame2()
+    }
+}
