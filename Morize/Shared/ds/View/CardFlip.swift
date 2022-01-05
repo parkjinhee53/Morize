645a82203d02b1a49b0eb5a8947bb1c5f13c0a3f
MojitoBar <wnehdtjr5@gmail.com>
2022-01-05 10:59:55 +0900

    Feat: Card Rotation Effect

--- Morize/Shared/ds/View/CardFlip.swift
+++ Morize/Shared/ds/View/CardFlip.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CardFlip: View {
     @ObservedObject var viewModel = CardFlipVM()
+    @State var flipped: [Bool] = [false, false, false, false]
     var body: some View {
         ZStack{
             ForEach(0..<viewModel.getWordsCount()) { i in
@@ -16,20 +17,25 @@ struct CardFlip: View {
                     RoundedRectangle(cornerRadius: 30)
                         .fill(LinearGradient(gradient: Gradient(colors: viewModel.getColor(index: i)), startPoint: .top, endPoint: .bottomTrailing))
                         .frame(width: 300, height: 250, alignment: .center)
-                        .shadow(color: .gray, radius: 25, x: -10, y: 10)
                     Text(viewModel.words[i])
                         .font(.system(size: 30, weight: .bold, design: .rounded))
                         .foregroundColor(.white)
                         .shadow(color: .black, radius: 2)
                 }
+                .rotation3DEffect(self.flipped[i] ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
                 .offset(viewModel.dragOffset[i])
+                .onTapGesture {
+                    // explicitly apply animation on toggle (choose either or)
+                    withAnimation {
+                        self.flipped[i].toggle()
+                    }
+                }
                 .gesture(
                     DragGesture()
                         .onChanged { gesture in
                             viewModel.dragOffset[i] = CGSize(width: gesture.translation.width, height: 0)
                         }
                         .onEnded { gesture in
-                            print(viewModel.dragOffset[i].width)
                             // 왼쪽인가 ? -150
                             if viewModel.dragOffset[i].width <= -130 {
                                 withAnimation(Animation.easeInOut(duration: 0.1)) {
