ab153a808e3df6305e6e977097a3202d929653b7
jinhee <parkjh6275@naver.com>
2022-01-06 09:06:34 +0900

    단어 리스트

--- Morize/Shared/jh/Views/WordList.swift
+++ Morize/Shared/jh/Views/WordList.swift
@@ -7,17 +7,36 @@
 
 import SwiftUI
 
+struct Middle: View {
+    var body: some View {
+        Text("apple")
+    }
+}
+struct High: View {
+    var body: some View {
+        Text("house")
+    }
+}
+struct TOEIC: View {
+    var body: some View {
+        Text("ice")
+    }
+}
+
 struct WordList: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("list")
-                    .padding()
-                    .font(.title)
-                Spacer()
-            }
-            Spacer()
+        List {
+            Section(header: Text("중등 영단어"), footer: Text("footer"), content: {
+                Middle()
+            })
+            Section(header: Text("고등 영단어"), footer: Text("footer"), content: {
+                High()
+            })
+            Section(header: Text("토익 영단어"), footer: Text("footer"), content: {
+                TOEIC()
+            })
         }
+        .listStyle(SidebarListStyle())
     }
 }
 
---
+++ Morize/Shared/jh/Views/WordList2.swift
@@ -0,0 +1,20 @@
+//
+//  WordList2.swift
+//  Morize
+//
+//  Created by Jinhee on 2022/01/04.
+//
+
+import SwiftUI
+
+struct WordList2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WordList2_Previews: PreviewProvider {
+    static var previews: some View {
+        WordList2()
+    }
+}
