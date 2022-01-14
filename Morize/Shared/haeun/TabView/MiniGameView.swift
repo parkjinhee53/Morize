540a7ab0d88c83c6f40620ba23ca510d9331cda6
hanni66 <haeunkim0807@naver.com>
2022-01-14 11:48:30 +0900

    Feat: tabviewsetting

--- Morize/Shared/haeun/TabView/MiniGameView.swift
+++ Morize/Shared/haeun/TabView/MiniGameView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct MiniGameView: View {
+    @State var state = 0
     var body: some View {
-        List{
-            Text("카드 맞추기")
-            Text("가로세로 낱말")
-            Text("타이머 게임")
+        VStack{
+            if state == 0 {
+                Button(action: {
+                    state = 1
+                }) {
+                    Text("카드 맞추기")
+                }.padding()
+                
+                Button(action: {
+                    state = 2
+                }) {
+                    Text("가로세로 낱말")
+                }.padding()
+                
+                Button(action: {
+                    state = 3
+                }) {
+                    Text("타이머 게임")
+                }.padding()
+            }
+            else if state == 1 {
+                MiniGame()
+            }
+            else if state == 2 {
+                minigame2()
+            }
+            else if state == 3 {
+                WelcomeView()
+            }
         }
     }
 }
