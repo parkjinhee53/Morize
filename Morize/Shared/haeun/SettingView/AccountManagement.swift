41f93dc018166d12473dea537a052a28dbe7ce02
hanni66 <haeunkim0807@naver.com>
2022-01-24 14:55:40 +0900

    Feat: setting view 구현

--- Morize/Shared/haeun/SettingView/AccountManagement.swift
+++ Morize/Shared/haeun/SettingView/AccountManagement.swift
@@ -10,8 +10,34 @@ import SwiftUI
 // 계정관리
 
 struct AccountManagement: View {
+    @AppStorage("UserName") var UserName : String = UserDefaults.standard.string(forKey: "UserName") ?? "사용자"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack{
+                profileImage()
+                Text(UserName)
+                    .bold()
+                    .font(.title)
+            }
+        }
+        
+    }
+}
+
+// 프로필 이미지
+struct profileImage: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Rectangle()
+                .foregroundColor(.green)
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 100)
+            Image("porfile")
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+        }
     }
 }
 
--- Morize/Shared/haeun/SettingView/SettingView.swift
+++ Morize/Shared/haeun/SettingView/SettingView.swift
@@ -8,65 +8,33 @@
 import SwiftUI
 
 struct SettingView: View {
-    @State var state = 0
     var body: some View {
-        VStack{
-            if state == 0{
-                Text("계정")
-                Button(action : {
-                    state = 1
-                }){
+        NavigationView {
+            ScrollView {
+                LabelledDivider(label: "계정")
+                NavigationLink(destination: AccountManagement()) {
                     Text("계정관리")
                 }
-                
-                Text("지원")
-                Button(action : {
-                    state = 2
-                }){
+                LabelledDivider(label: "지원")
+                NavigationLink(destination: NoticeView()) {
                     Text("공지사항 / 이벤트")
                 }
-                Button(action : {
-                    state = 3
-                }){
+                NavigationLink(destination: iCloudDataView()) {
                     Text("iCloud 동기화")
                 }
-                Button(action : {
-                    state = 4
-                }){
+                NavigationLink(destination: CSVDataView()) {
                     Text("CSV 파일 등록하기 / 내보내기")
                 }
-                
-                Text("문의")
-                Button(action : {
-                    state = 5
-                }){
+                LabelledDivider(label: "문의")
+                NavigationLink(destination: FAQView()) {
                     Text("자주 하는 질문")
                 }
-                Button(action : {
-                    state = 6
-                }){
+                NavigationLink(destination: SendEmailView()) {
                     Text("개발자에게 메일 보내기")
                 }
-                
             }
-            else if state == 1 {
-                AccountManagement()
-            }
-            else if state == 2 {
-                NoticeView()
-            }
-            else if state == 3 {
-                iCloudDataView()
-            }
-            else if state == 4 {
-                CSVDataView()
-            }
-            else if state == 5 {
-                FAQView()
-            }
-            else if state == 6 {
-                SendEmailView()
-            }
+        }.navigationBarHidden(true)
+        VStack{
             //로그인 정보가 들어있는 값에 따라서 카카오, 구글, 애플중에 고르기
             Button(action : {
                 
@@ -74,7 +42,32 @@ struct SettingView: View {
                 Text("로그아웃")
             }
         }
-        
+    }
+}
+
+// line 설정
+struct LabelledDivider: View {
+
+    let label: String
+    let horizontalPadding: CGFloat
+    let color: Color
+
+    init(label: String, horizontalPadding: CGFloat = 20, color: Color = .black) {
+        self.label = label
+        self.horizontalPadding = horizontalPadding
+        self.color = color
+    }
+
+    var body: some View {
+        HStack {
+            line
+            Text(label).foregroundColor(color)
+            line
+        }
+    }
+
+    var line: some View {
+        VStack { Divider().background(color) }.padding(horizontalPadding)
     }
 }
 
