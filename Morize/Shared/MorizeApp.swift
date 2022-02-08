500d3543e6b1fa6bc6f72e820ad394a91f713ec7
jinhee <parkjh6275@naver.com>
2022-02-08 09:28:49 +0900

    Feat: 게임 수정

--- Morize/Shared/MorizeApp.swift
+++ Morize/Shared/MorizeApp.swift
@@ -20,9 +20,11 @@ struct MorizeApp: App {
     }
     var body: some Scene {
         WindowGroup {
-            LoginView()
-                .environmentObject(self.userAuth)
-                .environmentObject(self.kakaoAuth)
+//            LoginView()
+//                .environmentObject(self.userAuth)
+//                .environmentObject(self.kakaoAuth)
+//            MiddleWord()
+            MainTabView()
         }
     }
 }
