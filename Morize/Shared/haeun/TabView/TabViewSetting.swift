831e4a016c6c71ec6f2893f6c6ee13ef1cb363bc
MojitoBar <wnehdtjr5@gmail.com>
2022-02-09 23:17:39 +0900

    Feat: 디자인 통일

--- Morize/Shared/haeun/TabView/TabViewSetting.swift
+++ Morize/Shared/haeun/TabView/TabViewSetting.swift
@@ -10,34 +10,49 @@ import SwiftUI
 struct TabViewSetting: View {
     @Binding var level: String
     var body: some View {
-        VStack {
-            // 상단 TabView
-            WordView()
-            // 하단 TabView
-            TabView {
-                CardMemory()
-                    .tabItem{ Text("Word")  }
-                    .navigationBarHidden(false)
-                
-                if level == "middle" {
-                    MiddleWord()
-                        .tabItem{ Text("List") }
+        ZStack {
+            Color(hex: "eaefe5")
+                .ignoresSafeArea()
+            VStack {
+                // 상단 TabView
+                WordView()
+                // 하단 TabView
+                TabView {
+                    CardMemory()
+                        .tabItem{ Text("Word")  }
+                        .navigationBarHidden(false)
+                    
+                    if level == "middle" {
+                        MiddleWord()
+                            .tabItem{
+                                Text("List")
+                            }
+                            .navigationBarHidden(false)
+                    }
+                    else if level == "high" {
+                        HighWord()
+                            .tabItem{
+                                Text("List")
+                            }
+                            .navigationBarHidden(false)
+                    }
+                    else if level == "toeic" {
+                        ToeicWord()
+                            .tabItem{
+                                Text("List")
+                            }
+                            .navigationBarHidden(false)
+                    }
+                    MiniGameView()
+                        .tabItem{ Text("Game") }
                         .navigationBarHidden(false)
                 }
-                else if level == "high" {
-                    HighWord()
-                        .tabItem{ Text("List") }
-                        .navigationBarHidden(false)
-                }
-                else if level == "toeic" {
-                    ToeicWord()
-                        .tabItem{ Text("List") }
-                        .navigationBarHidden(false)
-                }
-                MiniGameView()
-                    .tabItem{ Text("Game") }
-                    .navigationBarHidden(false)
             }
         }
+    }
+}
+struct TabViewSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewSetting(level: .constant("middle"))
     }
 }
